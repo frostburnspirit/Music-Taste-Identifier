5465aa660991a1079b5726af72ca248c4d786f81
frostburnspirit <frostburnspirit@gmail.com>
2023-04-04 09:55:18 +0200

    super basic layout stuff

---
+++ js/universal.js
@@ -0,0 +1,8 @@
+let header = document.getElementById("header");
+let footer = document.getElementById("footer");
+
+const nav = document.createElement("nav");
+nav.style.backgroundColor = "red";
+nav.style.width = "50px";
+nav.style.height = "50px";
+header.appendChild(nav);
