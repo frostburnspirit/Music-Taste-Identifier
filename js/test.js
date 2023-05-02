3d3150de1ca4825a728daab12621bf4e4b21492e
frostburnspirit <frostburnspirit@gmail.com>
2023-05-02 09:22:24 +0200

    working with section bar arrows

--- js/test.js
+++ js/test.js
@@ -1,41 +1,31 @@
 const testSections = document.querySelectorAll(".test-section");
 
 for (let i = 0; i < testSections.length; i++) {
-  const data = sectionData[i];
+  const DATA = sectionData[i];
 
-  let section = `
-    <section class="
-  
-  
-  `;
+  const TEST_SECTION = `
+    <div class="section-bar">
+      <h2>
+        ${DATA.title}
+      </h2>
+      <a class="section-bar-arrow fa-solid fa-angle-left"></a>
+    </div>
+    <div class="section-content">
+      ${DATA.text}
+    </div>`;
 
-  const tsBar = document.createElement("div"); // Skriv ut alla ord
-  const tsBarTitle = document.createElement("h2");
-  const tsBarButton = document.createElement("button");
-  const tsContent = document.createElement("div");
-
-  tsBar.classList.add("section-bar");
-  tsBarTitle.innerHTML = data.title;
-  tsBarButton.type = "button";
-  tsBar.append(tsBarTitle, tsBarButton);
-
-  tsContent.classList.add("section-content");
-  tsContent.innerHTML = data.text;
-  tsContent.style.color = "white";
-
-  testSections[i].append(tsBar, tsContent);
+  testSections[i].insertAdjacentHTML("beforeend", TEST_SECTION);
 }
 
-/*
+// console.log(document.getElementsByClassName("section-bar-arrow"));
+// document.getElementsByClassName("section-bar-arrow").forEach((element) => {
+//   element.addEventListener("click", function (element) {
+//     element.classList.add("arrow-open");
+//   });
+// });
 
-För rad 3-> 21 kan du använda en for loop på testSections så här
-*/
-
-testSections.forEach((index) => {
-  const data = sectionData[index];
-  // Och mer kod
-});
-
-/*
-Koden mellan 6 -> 9 kan man skapa elementet genom en sträng och gör det till en function
-*/
+for (const iterator of document.getElementsByClassName("section-bar-arrow")) {
+  iterator.addEventListener("click", function (e) {
+    iterator.classList.toggle("arrow-open");
+  });
+}
